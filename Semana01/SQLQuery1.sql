b79dc4868459a0bc0db95bb0ecf73b0fc0798fe6
Gustavo Coronel <gcoronelc@gmail.com>
2022-04-04 09:59:29 -0500

    Semana 1

--- Semana01/SQLQuery1.sql
+++ Semana01/SQLQuery1.sql
@@ -5,11 +5,23 @@ select * from Northwind..Products;
 go
 
 
+select *
+from Northwind..Categories C
+join Northwind..Products P on C.CategoryID = P.CategoryID;
+go
+
+
 select C.CategoryName, P.ProductName, P.UnitPrice, P.UnitsInStock
 from Northwind..Categories C
 join Northwind..Products P on C.CategoryID = P.CategoryID;
 go
 
+
+select C.CategoryName, count(P.ProductID) Productos, sum(P.UnitsInStock) Stock
+from Northwind..Categories C
+join Northwind..Products P on C.CategoryID = P.CategoryID
+group by C.CategoryName
+go
 
 
 
